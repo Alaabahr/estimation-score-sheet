cb1d1e2dd1d96c0e567c541591624e88637b8fb4
Alaabahr <Dida1981@hotmail.com>
2015-08-13 14:45:55 +0400

    Initial commit
    Initial commit

---
+++ EstimasionSS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EstimasionSS.Startup))]
+namespace EstimasionSS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
